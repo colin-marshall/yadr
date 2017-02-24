9d7f5807fbca7e3614adc9983d97e276002a6f61
Colin Marshall <colin.michael.marshall@gmail.com>
2017-02-24 16:19:29 -0700

    custom aliases & custom functions added

--- zsh/custom_aliases.zsh
+++ zsh/custom_aliases.zsh
@@ -5,6 +5,9 @@ alias vwp='cd ~/Sites/vagrant-local'
 alias projects='cd ~/Projects'
 alias forks='cd ~/Projects/Forks'
 
+# Clean and reinstall commands
+alias blowout-node='rm -rf node_modules && npm cache clean && npm install'
+
 # Directory/File & Sublime Text
 alias dotfiles='cd ~/.yadr && subl .'
 alias aliases='cd ~/.yadr && subl zsh/aliases.zsh'
@@ -18,4 +21,6 @@ alias ssh-buzz='ssh buzzreco@s6.fcomet.com -p 17177'
 # Variables
 alias ep='echo $PATH | tr -s ":" "\n"'
 
+# TotalFinder DMG Fix
+alias totalfinder-dmg-fix='killall TotalFinder && defaults write com.binaryage.totalfinder TotalFinderDoNotIgnoreDMG -bool true && killall Finder && open -a TotalFinder'
 
