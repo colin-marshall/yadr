45a1e639584ea93cec96143f39efa3ad7e9bdd63
Colin Marshall <colin.michael.marshall@gmail.com>
2017-08-30 13:06:41 -0600

    Added flac2alac function

--- zsh/custom_functions.zsh
+++ zsh/custom_functions.zsh
@@ -6,3 +6,15 @@ function render_spectrograms () {
 	    sox "$file" -n spectrogram -t "$title_in_pic" -o "$outfile"
 	done
 }
+
+function flac2alac() {	
+	if [[ $1 = "rm" ]] ; then
+		for f in ./*.flac; do
+			avconv -n -i "$f" -c:a alac "${f%.*}.m4a" && rm "$f";
+		done
+	else
+		for f in ./**/*.flac; do 
+			avconv -analyzeduration 500000000 -i "$f" -c:a alac "${f%.*}.m4a";
+		done
+	fi
+}
